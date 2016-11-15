5263c60643d6229dea294b0db7af33c331735a96
HP <krist.ng@corous360.com>
2016-11-15 17:58:20 +0800

    Initial commit

---
+++ app/src/main/java/com/questdot/slidingpaneexample/Bookmark.java
@@ -0,0 +1,14 @@
+package com.questdot.slidingpaneexample;
+
+/**
+ * Created by HP on 15/11/2016.
+ */
+public class Bookmark {
+    public String name;
+    public String link;
+
+	public Bookmark(String name, String link) {
+        this.name = name;
+        this.link = link;
+    }
+}
---
+++ app/src/main/java/com/questdot/slidingpaneexample/BookmarkListener.java
@@ -0,0 +1,8 @@
+package com.questdot.slidingpaneexample;
+
+/**
+ * Created by HP on 15/11/2016.
+ */
+public interface BookmarkListener {
+    public void onChangeBookmark(String bookmark);
+}
